eddd62bc5e8e4224341bff4cdb7a82d624d5e807
Rudey <rudey@steamhunters.com>
2018-07-09 20:42:46 +0200

    Update assembly info description and version numbers

--- EntityFramework.Utilities/EntityFramework.Utilities/Properties/AssemblyInfo.cs
+++ EntityFramework.Utilities/EntityFramework.Utilities/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RudeySH.EFUtilities")]
-[assembly: AssemblyDescription("Adds batch operations to Entity Framework when using MS Sql server. This a fork that adds various features and bugfixes to the original EFUtilities by Mikael Eliasson.")]
+[assembly: AssemblyDescription("Adds batch insert, delete by query, update by query and some other features to Entity Framework. This a fork that adds various features and bugfixes to the original EFUtilities by Mikael Eliasson.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RudeySH")]
 [assembly: AssemblyProduct("EntityFramework.Utilities")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]

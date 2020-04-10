58da9b9da627e7955fe9ee9099a9b81a9d2d9dd0
RudeySH <rudey@steamhunters.com>
2020-04-10 17:32:12 +0200

    Fix ComplexType property name resolution, fixes #25

---
+++ EntityFramework.Utilities/Tests/Models/ComplexTypeTestObject.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tests.Models
+{
+	public class ObjectWithComplexType
+	{
+		public int Id { get; set; }
+
+		public ComplexTypeObject ComplexType { get; set; } = new ComplexTypeObject();
+	}
+
+	[ComplexType]
+	public class ComplexTypeObject
+	{
+		public string Name { get; set; }
+
+		public AnotherComplexTypeObject Another { get; set; } = new AnotherComplexTypeObject();
+	}
+
+	[ComplexType]
+	public class AnotherComplexTypeObject
+	{
+		public string Name { get; set; }
+	}
+}
